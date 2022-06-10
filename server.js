5180193024009e16ca8ad67077717249c19481d2
jihyekang05 <62206966+jihyekang05@users.noreply.github.com>
2022-06-10 14:47:27 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -8,5 +8,12 @@ app.listen(8080,function(){
 app.get('/pet',function(req,res){
     res.send('반려동물 용품을 쇼핑할 수 있는 페이지입니다.')
 
+});
 
+app.get('/beauty',function(req,res){
+    res.send('화장품을 쇼핑할 수 있는 페이지입니다.')
+});
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html')
 });
